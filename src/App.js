2c0721bd2136afb092a6b9fdcbf94747d899337a
chudnofskyATD <mpereannor@gmail.com>
2019-09-16 13:48:54 +0000

    declaration of routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import {Route} from 'react-router-dom';
+
 
 const App = () => (
   <div>
--- src/index.js
+++ src/index.js
@@ -3,5 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+import {BrowserRouter as Router} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Router>
+    <App />
+    <Route exact path='/' component={Home}/>
+    <Route path='/about' component={About}/>
+    <Route path='/contact' component={Contact}/>
+
+    <Link to='/'> Home</Link>
+    <Link to='/about'>About</Link>
+    <Link to='/contact'></Link>
+</Router>, document.getElementById('root'));
