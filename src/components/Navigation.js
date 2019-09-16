8227fa402de7a4e5a314407e7328e02517b0cc66
chudnofskyATD <mpereannor@gmail.com>
2019-09-16 17:04:01 +0000

    nav project polished

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,15 +7,12 @@ const Navigation = (props) => {
       <div className="App">
         <h1>React Router Mini</h1>
         <div>
-          {/* <a href="">Home</a>  */}
           <Link to='/home'>Home</Link>
         </div>
         <div>
-          {/* <a href="">About</a> */}
           <Link to='/about'>About</Link>
         </div>
         <div>
-          {/* <a href="">Contact</a> */}
           <Link to='/contact'>Contact</Link>
         </div>
       </div>
